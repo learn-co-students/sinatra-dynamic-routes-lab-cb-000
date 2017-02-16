332d9756255a0297656168a5a299701af4fb821c
Jesus Vega <chuyspc@gmail.com>
2017-02-16 04:45:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -24,4 +24,22 @@ class App < Sinatra::Base
 
       new_phrase
     end
+
+    get "/say/:word1/:word2/:word3/:word4/:word5" do
+      phrase = [params[:word1] , params[:word2] , params[:word3] , params[:word4] , params[:word5]]
+      "#{phrase.join(" ")}."
+    end
+
+    get "/:operation/:number1/:number2" do
+      @num1 = params[:number1].to_i
+      @num2 = params[:number2].to_i
+      @op = params[:operation]
+      output = case op
+          when "multiply" then "#{@num1 * @num2}"
+          when "subtract" then "#{@num1 - @num2}"
+          when "divide" then "#{@num1 / @num2}"
+          when "add" then "#{@num1 + @num2}"
+        end
+        output
+    end
 end
