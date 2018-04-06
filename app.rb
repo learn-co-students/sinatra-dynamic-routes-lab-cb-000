73b29e9b32748973dc517c397ee06934d064fb83
Alex Segers <a.segers.dev@gmail.com>
2018-04-06 20:12:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,29 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name_reversed = params[:name].reverse
+    erb :'reverse_name.html'
+  end
+
+  get '/square/:number' do
+    @num_squared = params[:number].to_i ** 2
+    erb :'square.html'
+  end
+
+  get '/say/:number/:phrase' do
+    @phrases = params[:number].to_i.times.collect{params[:phrase]+"\n"}.join()
+    erb :'say_n_phrase.html'
+  end
+
+  get '/:operation/:number1/:number2' do
+    @result = params[:number1].to_i.public_send(params[:operation], params[:number2].to_i)
+    erb :'operation.html'
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @phrase = params.collect{|k, v| v if k.to_s.include?('word')}.join(' ')
+    erb :'say_five_words.html'
+  end
 
 end
