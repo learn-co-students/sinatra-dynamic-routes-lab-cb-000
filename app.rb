cd67b15e1e7a2229412518fc0af08fb9b6a0446b
Fer Hernandez <fh.1111@gmail.com>
2018-12-30 20:12:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,9 +8,39 @@ class App < Sinatra::Base
     "#{@name.reverse}"
   end
 
-  get "/multiply/:num1/:num2" do
-    @product = params[:num1].to_i * params[:num2].to_i
-    "#{@product}"
+  get "/square/:number" do
+    @number = params[:number].to_i
+    "#{@number * @number}"
+  end
+
+  get "/say/:number/:phrase" do
+    final_string = ""
+    params[:number].to_i.times do
+      final_string += "#{params[:phrase]}\n"
+    end
+    final_string
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if operation == "add"
+      answer = number1 + number2
+    elsif operation == "subtract"
+      answer = number1 - number2
+    elsif operation == "multiply"
+      answer = number1 * number2
+    elsif operation == "divide"
+      answer = number1 / number2
+
+    end
+    answer.to_s
+
   end
 
 end
